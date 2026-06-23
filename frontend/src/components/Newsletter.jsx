b5b8975fe42aef231aab50318d6223384f3bbaca
Dark Soul <rudrarudra150@gmail.com>
2026-06-23 09:32:45 +0530

    Making Newsletter Responsive

--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 const Newsletter = () => {
   return (
     <div className="flex flex-col items-center justify-center text-center space-y-2 max-md:px-4 my-10 mb-40">
@@ -23,4 +23,39 @@ const Newsletter = () => {
     </div>
   );
 };
+export default Newsletter;
+*/
+import React from "react";
+
+const Newsletter = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 my-10 mb-20 md:mb-40">
+      <h1 className="text-2xl md:text-4xl font-semibold">
+        Never Miss a Deal!
+      </h1>
+
+      <p className="text-sm md:text-lg text-gray-500/70 pb-6 md:pb-8 max-w-2xl">
+        Subscribe to get the latest offers, new arrivals, and exclusive
+        discounts
+      </p>
+
+      <form className="flex w-full max-w-2xl h-12 md:h-13">
+        <input
+          className="w-full h-full px-3 border border-gray-300 border-r-0 rounded-l-md outline-none text-gray-500"
+          type="email"
+          placeholder="Enter your email id"
+          required
+        />
+
+        <button
+          type="submit"
+          className="px-5 md:px-12 h-full text-white bg-primary hover:bg-primary-dull transition-all cursor-pointer rounded-r-md whitespace-nowrap"
+        >
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default Newsletter;
